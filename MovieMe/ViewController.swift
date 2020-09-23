9f38c9427b6df2208936f7e2e0617aa3b082a202
Meitar Basson <meitar.startup@gmail.com>
2020-09-23 23:08:16 +0300

    Project initalization

---
+++ MovieMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieMe
+//
+//  Created by Meitar Basson on 23/09/2020.
+//  Copyright © 2020 Meitar Basson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
